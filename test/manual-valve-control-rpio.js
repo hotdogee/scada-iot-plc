97870f3380b8680d04280a5ffd9d2e322b63aa52
Han Lin <hotdogee@gmail.com>
2019-04-08 02:15:42 +0800

    buttonState

---
+++ test/manual-valve-control-rpio.js
@@ -0,0 +1,34 @@
+// examples
+// node test/led-button-pigpio.js
+// node test/led-button-pigpio.js --led 12 --button 15
+
+// parse arguments
+const argv = require('minimist')(process.argv.slice(2), {
+  default: {
+    'relay': 18,
+    'button': 22
+  }
+})
+
+const Gpio = require('pigpio').Gpio
+// Turn the LED connected to GPIO17 on when the momentary push button
+// connected to GPIO4 is pressed. Turn the LED off when the button is
+// released.
+
+// state
+let valveState = 1
+let buttonState = 0
+
+const relay = new Gpio(argv.relay, {mode: Gpio.OUTPUT})
+const button = new Gpio(argv.button, {
+  mode: Gpio.INPUT,
+  pullUpDown: Gpio.PUD_DOWN,
+  edge: Gpio.EITHER_EDGE
+})
+
+button.on('interrupt', (level) => {
+  if (level === buttonState) return
+  buttonState = level
+  console.log(buttonState)
+  relay.digitalWrite(buttonState)
+})
