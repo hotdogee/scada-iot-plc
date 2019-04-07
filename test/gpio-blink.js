4ef6b97c8e99c3ab6b3200f19aa2992de1204d0f
Han Lin <hotdogee@gmail.com>
2019-04-08 01:09:20 +0800

    args

--- test/gpio-blink.js
+++ test/gpio-blink.js
@@ -1,23 +1,33 @@
+// parse arguments
+const argv = require('minimist')(process.argv.slice(2), {
+  default: {
+    'pin': 12,
+    'loop': 20,
+    'high': 1000,
+    'low': 500
+  }
+});
+
 const rpio = require('rpio')
 // Blink an LED attached to P12 / GPIO18 a few times
 /*
  * Set the initial state to low.  The state is set prior to the pin
  * being actived, so is safe for devices which require a stable setup.
  */
-rpio.open(12, rpio.OUTPUT, rpio.LOW)
+rpio.open(argv.pin, rpio.OUTPUT, rpio.LOW)
 
 /*
  * The sleep functions block, but rarely in these simple programs does
  * one care about that.  Use a setInterval()/setTimeout() loop instead
  * if it matters.
  */
-for (var i = 0; i < 20; i++) {
+for (var i = 0; i < argv.loop; i++) {
   /* On for 1 second */
   console.log(i)
-  rpio.write(12, rpio.HIGH)
-  rpio.sleep(1)
+  rpio.write(argv.pin, rpio.HIGH)
+  rpio.msleep(argv.high)
 
   /* Off for half a second (500ms) */
-  rpio.write(12, rpio.LOW)
-  rpio.msleep(500)
+  rpio.write(argv.pin, rpio.LOW)
+  rpio.msleep(argv.low)
 }
