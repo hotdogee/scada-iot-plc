dd452cc953eebf919aca9014e199dae78a81d92e
Han Lin <hotdogee@gmail.com>
2019-04-08 01:27:27 +0800

    style fix

--- test/button-poll.js
+++ test/button-poll.js
@@ -8,25 +8,27 @@ const argv = require('minimist')(process.argv.slice(2), {
     'pin': 15,
     'wait': 20
   }
-});
+})
 
 const rpio = require('rpio')
 // Poll a button switch for events
 // Configure the internal pullup resistor on P15 / GPIO22 and watch
 // the pin for pushes on an attached button switch:
-rpio.open(argv.pin, rpio.INPUT, rpio.PULL_UP);
+rpio.open(argv.pin, rpio.INPUT, rpio.PULL_UP)
 
 function pollcb(pin)
 {
   // Wait for a small period of time to avoid rapid changes which
   // can't all be caught with the 1ms polling frequency.  If the
   // pin is no longer down after the wait then ignore it.
-  rpio.msleep(argv.wait);
+  rpio.msleep(argv.wait)
 
   if (rpio.read(pin))
-    return;
+    return
 
-  console.log('Button pressed on pin P%d', pin);
+  console.log('Button pressed on pin P%d', pin)
 }
 
-rpio.poll(argv.pin, pollcb, rpio.POLL_DOWN);
+rpio.poll(argv.pin, pollcb, rpio.POLL_DOWN)
+
+// lags and crashes pi if pressed very fast
