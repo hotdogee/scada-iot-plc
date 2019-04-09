76f26ddd54500e567d37529ebb425def7617dfed
Han Lin <hotdogee@gmail.com>
2019-04-10 04:20:20 +0800

    temp sense

---
+++ test/temp-sense-pigpio.js
@@ -0,0 +1,31 @@
+// examples
+// sudo node test/led-button-pigpio.js
+// sudo node test/led-button-pigpio.js --led 12 --button 15
+
+// parse arguments
+const argv = require('minimist')(process.argv.slice(2), {
+  default: {
+    'led': 25,
+    'button': 24
+  }
+})
+
+const imu = require("node-sense-hat").Imu;
+
+const IMU = new imu.IMU();
+
+IMU.getValue((err, data) => {
+  if (err !== null) {
+    console.error("Could not read sensor data: ", err);
+    return;
+  }
+
+  console.log("Accelleration is: ", JSON.stringify(data.accel, null, "  "));
+  console.log("Gyroscope is: ", JSON.stringify(data.gyro, null, "  "));
+  console.log("Compass is: ", JSON.stringify(data.compass, null, "  "));
+  console.log("Fusion data is: ", JSON.stringify(data.fusionPose, null, "  "));
+
+  console.log("Temp is: ", data.temperature);
+  console.log("Pressure is: ", data.pressure);
+  console.log("Humidity is: ", data.humidity);
+});
