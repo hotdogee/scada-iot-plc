2bc3d63281707d68cbca7ad55f1d0f0b99cda493
Han Lin <hotdogee@gmail.com>
2019-04-10 04:53:24 +0800

    loop

--- test/temp-sense-pigpio.js
+++ test/temp-sense-pigpio.js
@@ -6,26 +6,33 @@
 const argv = require('minimist')(process.argv.slice(2), {
   default: {
     'led': 25,
-    'button': 24
+    'button': 24,
+    'loop': 200
   }
 })
 
-const imu = require("../../nodeimu");
+const util = require('util')
+const imu = require("../../nodeimu")
 
-const IMU = new imu.IMU();
+const IMU = new imu.IMU()
 
-IMU.getValue((err, data) => {
+let i = 0
+const callb = (err, data) => {
   if (err !== null) {
-    console.error("Could not read sensor data: ", err);
+    console.error("Could not read sensor data: ", err)
     return;
   }
+  // console.log("Accelleration is: ", JSON.stringify(data.accel, null, "  "));
+  // console.log("Gyroscope is: ", JSON.stringify(data.gyro, null, "  "));
+  // console.log("Compass is: ", JSON.stringify(data.compass, null, "  "));
+  // console.log("Fusion data is: ", JSON.stringify(data.fusionPose, null, "  "));
 
-  console.log("Accelleration is: ", JSON.stringify(data.accel, null, "  "));
-  console.log("Gyroscope is: ", JSON.stringify(data.gyro, null, "  "));
-  console.log("Compass is: ", JSON.stringify(data.compass, null, "  "));
-  console.log("Fusion data is: ", JSON.stringify(data.fusionPose, null, "  "));
+  // console.log("Temp is: ", data.temperature);
+  // console.log("Pressure is: ", data.pressure);
+  // console.log("Humidity is: ", data.humidity);
+  console.log(util.format('%s %s %s', data.temperature.toFixed(4), data.pressure.toFixed(4), data.humidity.toFixed(4)))
+  i += 1
+  if (i < argv.loop) setTimeout(function() { IMU.getValue(callb) }, 100)
+}
 
-  console.log("Temp is: ", data.temperature);
-  console.log("Pressure is: ", data.pressure);
-  console.log("Humidity is: ", data.humidity);
-});
+IMU.getValue(callb)
