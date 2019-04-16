27b7f3ba34ad6ca5d965ede1b5919674a22a777a
Han Lin <hotdogee@gmail.com>
2019-04-16 17:24:24 +0800

    fix

--- login.js
+++ login.js
@@ -1,13 +1,11 @@
 require('dotenv').config()
 const feathers = require('feathers/client')
 const socketio = require('feathers-socketio/client')
-const hooks = require('feathers-hooks')
+// const hooks = require('feathers-hooks')
 // const errors = require('feathers-errors') // An object with all of the custom error types.
 const auth = require('feathers-authentication-client')
 const io = require('socket.io-client')
 const config = require('config')
-// const localStorage = require('node-persist')
-const { createLocalStorage } = require('localstorage-ponyfill')
 const { createLogger, format, transports } = require('winston')
 const logger = createLogger({
   level: 'debug',
@@ -21,29 +19,32 @@ const logger = createLogger({
     new transports.Console()
   ]
 })
+// const Storage = require('dom-storage')
+// var localStorage = new Storage('./localStorage.json')
+const localStorage = require('node-persist')
+localStorage.initSync()
+localStorage.setItem = localStorage.setItemSync
+localStorage.getItem = localStorage.getItemSync
+
+const ioConfig = config.get('supervisor')
+logger.info(`Connecting to feathers server: `, ioConfig)
+const socket = io(ioConfig.url, ioConfig.options)
+
+const supervisor = feathers()
+  .configure(socketio(socket))
+  // .configure(hooks())
+  .configure(
+    auth({
+      storage: localStorage
+    })
+  )
 
 const email = process.env.USERNAME || 'user@example.com'
 const password = process.env.PASSWORD || 'random!password'
 logger.info(`User ${email}, Password ${password}`)
 
-;(async function () {
+;(async function (supervisor) {
   try {
-    // const Storage = require('dom-storage')
-    // var localStorage = new Storage('./localStorage.json')
-    // const localStorage = createLocalStorage({ mode : "node", storeFilePath: "./localstorage" })
-    // await localStorage.init()
-    
-    const ioConfig = config.get('supervisor')
-    logger.info(`Connecting to feathers server: `, ioConfig)
-    const socket = io(ioConfig.url, ioConfig.options)
-    
-    const supervisor = feathers()
-      .configure(socketio(socket))
-      .configure(hooks())
-      .configure(auth({
-        // storage: localStorage
-      }))
-    
     const token = await supervisor.authenticate({
       strategy: 'local',
       email,
@@ -55,4 +56,4 @@ logger.info(`User ${email}, Password ${password}`)
   } finally {
     process.exit()
   }
-})()
+})(supervisor)
