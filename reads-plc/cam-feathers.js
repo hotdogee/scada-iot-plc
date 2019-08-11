ee6b7cbd5a76689eca16f673ce9542963a964f73
Han Lin <hotdogee@gmail.com>
2019-08-11 15:38:18 +0800

    wip implementation of cam image upload

--- reads-plc/cam-feathers.js
+++ reads-plc/cam-feathers.js
@@ -1,5 +1,9 @@
 // node amqp-feathers.js --supervisor=goo.bio:3030 --ampqstr=amqp://localhost
 //
+// Steps
+// - Get jpeg from camera
+// - Upload to supervisor
+//
 // Network
 // - Network disconnect/reconnect event handling
 //
@@ -26,13 +30,80 @@
 // Write to csv file (10MB per file with total max size limit 8GB)
 // Send to RabbitMQ where workers try to send to server
 // (no data loss if internet fail, minimum data loss if power failure)
-const config = require('config')
 // parse arguments
 const argv = require('minimist')(process.argv.slice(2), {
   default: {
-    'supervisor': config.get('supervisor.url')
+    'supervisorUrl': process.env.API_URL,
+    'cam1PhotoUrl': process.env.CAM1_PHOTO_URL
   }
 })
+const fs = require('fs')
+// jpeg, 2048x1536
+const request = require('request')
+
+const blobService = 'https://scada.hanl.in/api' + '/blob'
+const cam1 = 'http://plc:5ZHGwbbShBEzG2Kv@192.168.2.85/Streaming/channels/3/picture'
+const req = request
+  .get('http://plc:5ZHGwbbShBEzG2Kv@192.168.2.85/Streaming/channels/1/picture')
+  .on('response', function (response) {
+    if (response.statusCode === 200) {
+      console.log(response.statusCode) // 200
+      console.log(response.headers['content-type']) // 'image/png'
+      const formData = {
+        // Pass a simple key-value pair
+        type: 'image',
+        metadata: {
+          name: 'cam1'
+        },
+        timestamp: new Date(),
+        file: req
+      }
+      request.post({ url: blobService, formData }, function (err, httpResponse, body) {
+        if (err) {
+          return console.error('upload failed:', err)
+        }
+        console.log('Upload successful!  Server responded with:', body)
+      })
+    } else {
+      console.error(response.statusCode) // 200
+      console.error(response.headers['content-type']) // 'image/png'
+    }
+  })
+  .on('error', function (err) {
+    console.error(err)
+  })
+
+let req = request.get('http://192.168.2.85/Streaming/channels/1/picture').on('end', () => {console.log('end')}).on('response', function (response) { console.log(response.statusCode); console.log(response.headers['content-type']); if (response.statusCode === 200) req.pipe(fs.createWriteStream('cam3.jpg'))}).on('error', function (err) { console.error(err) })
+request.get(cam1).on('end', () => {console.log('end')}).on('response', function (response) { console.log(response.statusCode); console.log(response.headers['content-type']) }).on('error', function (err) { console.error(err) })
+
+request.get('http://192.168.2.85/Streaming/channels/1/picture').on('response', function (response) { console.log(response.statusCode); if (response.statusCode !== 200) this.pause(); console.log(response.headers['content-type']) }).on('error', function (err) { console.error(err) }).pipe(fs.createWriteStream('cam1.jpg'))
+
+request('http://plc:5ZHGwbbShBEzG2Kv@192.168.2.85/Streaming/channels/2/picture').pipe(fs.createWriteStream('cam1.jpg'))
+
+request('http://plc:5ZHGwbbShBEzG2Kv@192.168.2.85/Streaming/channels/3/picture').pipe(fs.createWriteStream('cam1.jpg'))
+
+request(cam1, function (error, response, body) {
+  console.error('error:', error) // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
+  // console.log('body:', body); // Print the HTML for the Google homepage.
+})
+
+const formData = {
+  // Pass a simple key-value pair
+  type: 'image',
+  metadata: {
+    name: 'cam1'
+  },
+  timestamp: new Date(),
+  file: request(cam1)
+}
+request.post({ url: supervisorUrl, formData }, function (err, httpResponse, body) {
+  if (err) {
+    return console.error('upload failed:', err)
+  }
+  console.log('Upload successful!  Server responded with:', body)
+})
+
 const feathers = require('feathers/client')
 const socketio = require('feathers-socketio/client')
 const hooks = require('feathers-hooks')
